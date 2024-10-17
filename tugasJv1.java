223bde27b1af71b28186355955aeab0273c29c6e
k <159992286+cleariv@users.noreply.github.com>
2024-10-17 19:58:19 +0700

    Add files via upload

---
+++ tugasJv1.java
@@ -0,0 +1,15 @@
+// SOAL: 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32
+
+package tugasPerluangan;
+
+public class tugasJv1 {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <=32; i +=2) {
+            System.out.println(i + " ");
+        }
+
+    }
+}
+
+// OUTPUT: 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32
